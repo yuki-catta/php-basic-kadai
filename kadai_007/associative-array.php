e9db3f36367c2656f3080ceaa39b3005d903ee56
yuki-catta <144863087+yuki-catta@users.noreply.github.com>
2024-03-19 22:08:36 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>Pkadai_007</title>
+ </head>
+ 
+ <body>
+  <p>
+    <?php 
+      $associative = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($associative);
+    ?>
+  </p>
+ </body>
+ 
+ </html>
